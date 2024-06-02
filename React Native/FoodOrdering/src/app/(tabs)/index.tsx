852065bf842f8b50637546dca650a4467ad1cfda
unknown <sashenhasindu@gmail.com>
2024-06-02 23:15:41 +1000

    Building UI

--- React Native/FoodOrdering/src/app/(tabs)/index.tsx
+++ React Native/FoodOrdering/src/app/(tabs)/index.tsx
@@ -1,35 +1,37 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Colors from "@/src/constants/Colors";
+import products from "@/assets/data/products";
 
-import EditScreenInfo from "@/src/components/EditScreenInfo";
-import { Text, View } from "@/src/components/Themed";
+const product = products[5];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image style={styles.image} source={{ uri: product.image }} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
   },
   title: {
-    fontSize: 20,
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
     fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+
+  image: {
+    width: "100%",
+    aspectRatio: 1,
   },
 });
